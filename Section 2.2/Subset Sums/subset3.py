f37dccd4800df8f33c34585af16b26773a7d5e6b
Pranav Ramesh <47484624+pr28416@users.noreply.github.com>
2020-01-27 20:43:07 -0500

    Worked on Subset Sums

--- Section 2.2/Subset Sums/subset3.py
+++ Section 2.2/Subset Sums/subset3.py
@@ -4,10 +4,10 @@ LANG: PYTHON3
 TASK: subset
 """
 
-N = 0
+n = 0
 answer = 0
 with open("subset.in", "r") as f:
-    N = int(f.readline())
+    n = int(f.readline())
 
 def printDP(dp):
     for i in dp:
@@ -18,9 +18,10 @@ def printDP(dp):
 allSets = set()
 
 def span(dp, node, totalSum, currentSet):
+    global n
     # Check to see if 0 is the current spot. If so, add the currentSet to allSets
     if dp[node][totalSum] == 0:
-        if sum(currentSet) == N*(N+1)//4:
+        if sum(currentSet) == n*(n+1)//4:
             allSets.add(tuple(currentSet))
         return
 
@@ -37,29 +38,43 @@ def span(dp, node, totalSum, currentSet):
         # Span up
         span(dp, node-1, totalSum, currentSet.copy())
 
-if (N*(N+1))//2 % 2 == 0:
+def populate(N):
+    if (N*(N+1))//2 % 2 == 0:
 
-    req = (N*(N+1))//4
+        req = (N*(N+1))//4
 
-    # PART 1: Create the dp 2d array
-    dp = [[-1 for i in range(req+1)] for j in range(N+1)]
+        # PART 1: Create the dp 2d array
+        dp = [[-1 for i in range(req+1)] for j in range(N+1)]
 
-    # PART 2: Set the values of each DP position
-    for node in range(N+1):
-        for totalSum in range(req+1):
-            # Values that are seen
-            seenValues = [i for i in range(node+1)]
-            
-            # Check to see what value can be put in the box
-            if sum(seenValues) > totalSum:
-                dp[node][totalSum] = totalSum
-            else:
-                dp[node][totalSum] = sum(seenValues)
-                break
+        # PART 2: Set the values of each DP position
+        for node in range(N+1):
+            for totalSum in range(req+1):
+                # Values that are seen
+                seenValues = [i for i in range(node+1)]
+                
+                # Check to see what value can be put in the box
+                if sum(seenValues) > totalSum:
+                    dp[node][totalSum] = totalSum
+                else:
+                    dp[node][totalSum] = sum(seenValues)
+                    break
 
-    # PART 3: Starting from the bottom-right corner, create sets
-    span(dp, N, req, set())
+        # PART 3: Starting from the bottom-right corner, create sets
+        allSets.clear()
+        span(dp, N, req, set())
+        # c = 0
+        # for node in range(N, -1, -1):
+        #     for totalSum in range(req, -1, -1):
+        #         if dp[node][totalSum] == req:
+        #             c += 1
+        
+        answer = len(allSets)//2
+        print("%s: %s" % (N, answer))
+    else:
+        print("%s: \\\\" % N)
 
-    # for i in allSets:
-    #     print(i)
-    print(len(allSets)//2)
+# for i in range(2, 10):
+#     populate(i)
+populate(n)
+with open("subset.out", "w") as f:
+    f.write("%s\n" % answer)
