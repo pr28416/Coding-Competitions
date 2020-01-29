0404000e9a2d02d70c6d92fa91a60d45da4d0ac2
Pranav Ramesh <47484624+pr28416@users.noreply.github.com>
2020-01-29 16:51:28 -0500

    Worked on C++ Subset Sums

--- Section 2.2/Subset Sums/subset.cpp
+++ Section 2.2/Subset Sums/subset.cpp
@@ -7,17 +7,40 @@ LANG: C++
 #include <fstream>
 #include <string>
 
+std::string allSets[] = {};
+int r = 0;
+int c = 0;
+
 int populate(int n) {
   int answer = 0;
   if (n*(n+1)/2 % 2 == 0) {
-    return n;
+    int req = (n*(n+1))/4;
+
+    // PART 1: Create the dp 2d array, set the values of each DP position
+    int dp[n+1][req+1];
+    for (int i = 0; i < n+1; i++) {
+      for (int j = 0; j < req+1; j++) {
+        dp[i][j] = -1;
+      }
+    }
+
+    // PART 2: Starting from the bottom-right corner, create sets
+    span();
+
   } else {
     return 0;
   }
 }
 
+void span(int dp[r][c], int node, int totalSum, int currentSet[]) {
+
+  return;
+}
+
 int main() {
-  int n = 7;
-  std::cout<<populate(n)<<"\n";
+  std::ifstream fin ("subset.in");
+  int n;
+  fin >> n;
+  std::cout<<n<<"\t"<<populate(n)<<"\n";
   return 0;
 }
