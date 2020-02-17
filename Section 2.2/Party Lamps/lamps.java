c8b61112f01b1ae06f0c82e3863effc78973b790
Pranav Ramesh <47484624+pr28416@users.noreply.github.com>
2020-02-17 13:33:16 -0500

    Memoized Party Lamps

--- Section 2.2/Party Lamps/lamps.java
+++ Section 2.2/Party Lamps/lamps.java
@@ -11,9 +11,25 @@ public class lamps {
     static int C;
     static String finalState = "";
     static HashMap<String, String> allSets= new HashMap<String, String>();
+    static HashMap<String, Integer> used = new HashMap<String, Integer>();
     public static void main(String[] args) throws IOException {
-        // System.out.println("Started");
-        BufferedReader reader = new BufferedReader(new FileReader(new File("lamps.in")));
+        runProgram();
+    }
+
+    // public static void debug() throws IOException {
+    //     String t = "11111111";
+    //     System.out.println("Before: "+t);
+    //     t = button1(t);
+    //     t = button3(t);
+    //     t = button2(t);
+    //     t = button4(t);
+    //     System.out.println("After: "+t);
+    // }
+
+    public static void runProgram() throws IOException {
+        
+        System.out.println("Started");
+        BufferedReader reader = new BufferedReader(new FileReader(new File("lamps3.in")));
         PrintWriter writer = new PrintWriter(new File("lamps.out"));
 
         N = Integer.parseInt(reader.readLine());
@@ -38,27 +54,33 @@ public class lamps {
             finalState += i;
         }
 
-        // System.out.println(Arrays.toString(finalState));
+
         String bulbState = "";
         for (int i = 0; i < N; i++) {
             bulbState += "1";
         }
-        // System.out.println("Final State: "+ finalState+", Bulb State: "+bulbState);
+
+        long startTime = System.currentTimeMillis();
         span(bulbState, 0);
-        // System.out.println("Final:");
-        // for (String i:allSets) {
-        //     System.out.println(i);
-        // }
+        long endTime = System.currentTimeMillis();
+        System.out.println(String.format("Program took %s seconds", (endTime-startTime)/1000.0));
+
         if (allSets.size() == 0) {
             writer.println("IMPOSSIBLE");
         } else {
+            ArrayList<String> sets = new ArrayList<String>();
             for (String i: allSets.keySet()) {
+                sets.add(i);
+            }
+            Collections.sort(sets);
+            for (String i: sets) {
                 writer.println(i);
             }
         }
         
         reader.close();
         writer.close();
+        
     }
 
     public static String button1(String s) {
@@ -106,6 +128,11 @@ public class lamps {
     }
 
     public static void span(String bulbState, int c) {
+        for (int i = c; i < C; i += 4) {
+            if (used.containsKey(""+i+"-"+bulbState)) {
+                return;
+            }
+        }
         if (c == C) {
             boolean noFind = true;
             for (int i = 0; i < bulbState.length(); i++) {
@@ -127,33 +154,23 @@ public class lamps {
             // System.out.println("Worked "+bulbState);
             allSets.put(bulbState, bulbState);
             return;
+        } else {
+            used.put(""+c+"-"+bulbState, c);
         }
+        // // Button 2+3
+        // String b23 = button2(button3(bulbState));
+        // span(b23, c+2);
         // Button 1
-        // int[] b1 = bulbState.clone();
-        // for (int i = 0; i < b1.length; i++) {
-        //     b1[i] = (b1[i]+1)%2;
-        // }
         String b1 = button1(bulbState);
         span(b1, c+1);
         // Button 2
-        // int[] b2 = bulbState.clone();
-        // for (int i = 0; i < b2.length; i+=2) {
-        //     b2[i] = (b2[i]+1)%2;
-        // }
         String b2 = button2(bulbState);
         span(b2, c+1);
         // Button 3
-        // int[] b3 = bulbState.clone();
-        // for (int i = 1; i < b3.length; i+=2) {
-        //     b3[i] = (b3[i]+1)%2;
-        // }
         String b3 = button3(bulbState);
         span(b3, c+1);
+        
         // Button 4
-        // int[] b4 = bulbState.clone();
-        // for (int i = 0; i < b4.length; i+=3) {
-        //     b4[i] = (b4[i]+1)%2;
-        // }
         String b4 = button4(bulbState);
         span(b4, c+1);
         // After
