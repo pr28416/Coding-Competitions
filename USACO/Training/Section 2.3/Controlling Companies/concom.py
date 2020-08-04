dd1602fcce945993529b88e5a32c4ec94f572d2a
Pranav Ramesh <47484624+pr28416@users.noreply.github.com>
2020-08-04 13:24:02 -0400

    Worked on concom

--- USACO/Training/Section 2.3/Controlling Companies/concom.py
+++ USACO/Training/Section 2.3/Controlling Companies/concom.py
@@ -10,5 +10,26 @@ controlGrid = [[0] * N for i in range(N)]
 for i in c:
     controlGrid[i[0]-1][i[1]-1] = i[2]
 
-for i in controlGrid:
-    print(i)
+for k, i in enumerate(controlGrid):
+    print(k+1, end="|  ")
+    for j in i:
+        print(j, end="\t")
+    print()
+
+
+pairs = {}
+
+# Step 1: Count num pairs > 50
+for i in range(len(controlGrid)):
+    for j in range(len(controlGrid)):
+        if i != j and controlGrid[i][j] > 50:
+            if i+1 in pairs:
+                pairs[i+1].append(j+1)
+            else:
+                pairs[i+1] = [j+1]
+
+# Step 2: Assign additional percentages
+
+
+
+print(*pairs.items())
