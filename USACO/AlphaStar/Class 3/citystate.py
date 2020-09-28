eee8045525e9e592fc39327b7a314878f8794849
Pranav Ramesh <pranav.ramesh1@gmail.com>
2020-09-28 12:37:39 -0400

    Finished citystate.py

--- USACO/AlphaStar/Class 3/citystate.py
+++ USACO/AlphaStar/Class 3/citystate.py
@@ -1,46 +1,28 @@
 N = int(input())
-pairs = []
-inverse = []
-for _ in range(N):
-    e = input().split(" ")
-    pairs.append(e)
-    inverse.append(e[::-1])
+places = ["".join([i[:2] for i in input().split(" ")]) for _ in range(N)]
+places.sort()
 
-alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-abv = [f"{i}{j}" for i in alpha for j in alpha]
-matrix = [[0]*676 for _ in range(676)]
-# print(abv)
-
-def bs(x, lst, i, j, comp):
-    lo, up = i, j
+def bs(x, lst, comp):
+    lo, up = 0, len(lst)
     while lo < up:
         y = lo+(up-lo)//2
-        if comp(x, lst[y]): up = y
-        else: lo = y+1
+        if comp(x, lst[y]): up=y
+        else: lo=y+1
     return lo
 
-pairs.sort()
-inverse.sort()
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+abv = [f"{alpha[i]}{alpha[j]}" for i in range(26) for j in range(26)]
+matrix = [[0] * 676 for _ in range(676)]
 
-# for i in pairs: print(i)
-for pair in pairs:
-    # print(pair)
-    olo = bs(pair[1], pairs, 0, len(pairs), lambda x, y: x <= y[0][:2])
-    oup = bs(pair[1], pairs, 0, len(pairs), lambda x, y: x < y[0][:2])
-    # print(f"\t{pair} possible choices ({lo}:{up}): {pairs[lo:up]}")
-    lo = bs(pair[0][:2], inverse, 0, len(pairs), lambda x, y: x <= y[0])
-    up = bs(pair[0][:2], inverse, 0, len(pairs), lambda x, y: x < y[0])
-    # print(f"\t{pair} narrowed down to ({lo}:{up}): {pairs[lo:up]}")
-    if lo < up:
-        print(f"\t{pair} narrowed down to ({lo}:{up}): {inverse[lo:up]}")
-        i, j = bs(pair[0][:2], abv, 0, len(abv), lambda x, y: x <= y), bs(pair[1], abv, 0, len(abv), lambda x, y: x <= y)
-        matrix[i][j] += up-lo
-        # matrix[j][i] += up-lo
-        # print(f"\tADDING pairs ({abv[i]},{abv[j]}) and ({abv[j]},{abv[i]})")
+for place in places:
+    rev = place[2:] + place[:2]
+    lo, up = bs(rev, places, lambda x, y: x <= y), bs(rev, places, lambda x, y: x < y)
+    if lo == up: continue
+    i, j = bs(place[:2], abv, lambda x, y: x <= y), bs(place[2:], abv, lambda x, y: x <= y)
+    matrix[i][j] += up-lo
 
 s = 0
-for i in range(len(matrix)):
-    for j in range(i+1, len(matrix)):
+for i in range(676):
+    for j in range(i+1, 676):
         s += matrix[i][j]
-
 print(s)
