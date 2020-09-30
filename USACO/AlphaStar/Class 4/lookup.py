fb2c801e5d7264d420ff4f4352051b8dd66411e3
Pranav Ramesh <pranav.ramesh1@gmail.com>
2020-09-30 17:07:01 -0400

    Started shoppay.py

--- USACO/AlphaStar/Class 4/lookup.py
+++ USACO/AlphaStar/Class 4/lookup.py
@@ -1,11 +1,9 @@
 N = int(input())
 cows = [int(input()) for _ in range(N)]
-table = []
-stack = []
+table, stack = [], []
 for i in range(len(cows)-1, -1, -1):
     while len(stack) > 0 and cows[stack[-1]] <= cows[i]: stack.pop()
     if len(stack) == 0: table.append(0)
     else: table.append(stack[-1]+1)
     stack.append(i)
-
 for i in range(len(table)-1, -1, -1): print(table[i])
