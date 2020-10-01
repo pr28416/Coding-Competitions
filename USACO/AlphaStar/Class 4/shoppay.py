efeb779ef221c15d39dfaa2c99848e0d2702d4a7
Pranav Ramesh <pranav.ramesh1@gmail.com>
2020-09-30 21:31:58 -0400

    Finished shoppay.py

--- USACO/AlphaStar/Class 4/shoppay.py
+++ USACO/AlphaStar/Class 4/shoppay.py
@@ -1,1 +1,17 @@
+from queue import Queue
 N = int(input())
+registers = [list() for _ in range(N)]
+
+waitingLine = Queue()
+while True:
+    try: process = input()
+    except: break
+    if not process: break
+    process = process.split(" ")
+    mode, n = process[0], int(process[1])
+    if mode == "C": waitingLine.put(n)
+    else:
+        if not waitingLine.empty():
+            registers[n-1].append(waitingLine.get_nowait())
+
+for r in registers: print(len(r), *r)
