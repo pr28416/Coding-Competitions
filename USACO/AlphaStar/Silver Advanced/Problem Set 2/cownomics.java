49312253fb92331893b8076e0c98fe9cccdc43ed
Pranav Ramesh <pranav.ramesh1@gmail.com>
2020-11-01 10:05:46 -0500

    Finished adv 2.2

---
+++ USACO/AlphaStar/Silver Advanced/Problem Set 2/cownomics.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class cownomics {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt(), M = input.nextInt();
+        String[] spotty = new String[N], plain = new String[N];
+        for (int i = 0; i < N; i++) spotty[i] = input.next();
+        for (int i = 0; i < N; i++) plain[i] = input.next();
+        input.close();
+        int count = 0;
+        for (int i = 0; i < M-2; i++) {
+            for (int j = i+1; j < M-1; j++) {
+                outerloop: for (int k = j+1; k < M; k++) {
+                    int[] marked = new int[64];
+                    for (String s: spotty) {
+                        marked[conv(s.charAt(i))*16+conv(s.charAt(j))*4+conv(s.charAt(k))] = 1;
+                    }
+                    for (String s: plain) {
+                        if (marked[conv(s.charAt(i))*16+conv(s.charAt(j))*4+conv(s.charAt(k))] == 1) continue outerloop;
+                    }
+                    count += 1;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static int conv(char c) {
+        if (c == 'A') return 0;
+        if (c == 'C') return 1;
+        if (c == 'G') return 2;
+        return 3;
+    }
+}
