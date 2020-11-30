7be821bb6cda2e5e34264e449d424a6ac24dfb57
Pranav Ramesh <pranav.ramesh1@gmail.com>
2020-11-29 20:52:17 -0500

    Finished p3, refactored p1

--- USACO/AlphaStar/Silver Advanced/Problem Set 5/measurement.java
+++ USACO/AlphaStar/Silver Advanced/Problem Set 5/measurement.java
@@ -1,109 +1,85 @@
 import java.util.*;
-import java.io.*;
+
 public class measurement {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         // Get data
         Scanner input = new Scanner(System.in);
-        // Scanner input = new Scanner(new File("measurement.in"));
+
         int N = input.nextInt(), G = input.nextInt();
         int[][] entries = new int[N][3];
         for (int i = 0; i < N; i++) {
-            entries[i] = new int[]{input.nextInt(),
-                                    input.nextInt(),
-                                    input.nextInt()};
+            entries[i][0] = input.nextInt();
+            entries[i][1] = input.nextInt();
+            entries[i][2] = input.nextInt();
         }
         input.close();
+
         Arrays.sort(entries, new Comparator<int[]>() {
             public int compare(final int[] a, final int[] b) {
                 return a[0] - b[0];
             }
         });
 
-        // for (int[] i: entries) System.out.println(Arrays.toString(i));
-
         // Initial setup of production and leaderboard maps
         HashMap<Integer, Integer> production = new HashMap<Integer, Integer>();
         production.put(0, G);
         for (int[] i: entries) production.put(i[1], G);
         
-        TreeMap<Integer, HashSet<Integer>> leaderboard = new TreeMap<Integer, HashSet<Integer>>();
-        HashSet<Integer> initial = new HashSet<Integer>();
-        for (int[] i: entries) initial.add(i[1]);
-        initial.add(0);
-        leaderboard.put(G, initial);
-        initial = null;
-        input = null;
-        
-        // System.out.println(production);
-        // System.out.println(leaderboard);
+        TreeMap<Integer, Integer> leaderboard = new TreeMap<Integer, Integer>();
+        leaderboard.put(G, N);
 
         // Run through entries
         int change = 0;
 
-        for (int[] entry: entries) {
-            // boolean removeChange = false;
-            // boolean insertChange = false;
+        int initialMaxKey, initialMaxKeyLength;
+        int initProdValue, newProdValue;
+
+        for (int e = 0; e < entries.length; e++) {
 
             // Store initial max-key and its length
-            int initialMaxKey = leaderboard.lastKey();
-            int initialMaxKeyLength = leaderboard.get(initialMaxKey).size();
-            int initialSingleValue = -1;
-            if (initialMaxKeyLength == 1) initialSingleValue = leaderboard.get(initialMaxKey).iterator().next();
+            initialMaxKey = leaderboard.lastKey();
+            initialMaxKeyLength = leaderboard.get(initialMaxKey);
 
             // PART 1: Remove old value from leaderboard and production
 
             // Remove the number from leaderboard
-            int initProdValue = production.get(entry[1]);
-            leaderboard.get(initProdValue).remove(entry[1]);
+            initProdValue = production.get(entries[e][1]);
+            leaderboard.put(initProdValue, leaderboard.get(initProdValue)-1);
 
             // Remove entire object if necessary
-            if (leaderboard.get(initProdValue).isEmpty()) {
+            if (leaderboard.get(initProdValue) == 0) {
                 leaderboard.remove(initProdValue);
             }
             
             // PART 2: Update new values for production and leaderboard
             
             // Change production value to new value
-            production.put(entry[1], entry[2] + production.get(entry[1]));
+            production.put(entries[e][1], entries[e][2] + production.get(entries[e][1]));
             
             // Create new object for update value if necessary
-            int newProdValue = production.get(entry[1]);
+            newProdValue = production.get(entries[e][1]);
             if (!leaderboard.containsKey(newProdValue)) {
-                leaderboard.put(newProdValue, new HashSet<Integer>());
+                leaderboard.put(newProdValue, 0);
             }
             
-            // Add new value (entry key) to appropriate key (new prod value)
-            leaderboard.get(newProdValue).add(entry[1]);
+            // Add new value (entries[e] key) to appropriate key (new prod value)
+            leaderboard.put(newProdValue, leaderboard.get(newProdValue)+1);
 
             // PART 3: Check for changes
 
             // Check if max changed after removal
             int last = leaderboard.lastKey();
             if (last != initialMaxKey) {
-                int newLast = leaderboard.get(last).iterator().next();
-                if (leaderboard.get(last).size() > 1 || newLast != initialSingleValue) {
+                if (leaderboard.get(last) > 1) {
                     change += 1;
-                    // System.out.println("New max: " + leaderboard.lastKey());
-                } else {
-                    // System.out.printf("There was new max but values were the same: %s and %s (from %s)\n", last, newLast, leaderboard.get(last));
+                } else if (initialMaxKeyLength != leaderboard.get(last) || !(newProdValue == last && initProdValue == initialMaxKey)) {
+                    change += 1;
                 }
-            }
-            // Otherwise, check if max length changed
-            else if (leaderboard.get(leaderboard.lastKey()).size() != initialMaxKeyLength) {
+            } else if (leaderboard.get(last) != initialMaxKeyLength) {
                 change += 1;
-                // System.out.println("Size of max changed: " + initialMaxKeyLength + " --> " + leaderboard.get(leaderboard.lastKey()).size());
-            } else {
-                // System.out.println("No change");
             }
-
-            // System.out.println("\tproduction: " + production);
-            // System.out.println("\tleaderboard: " + leaderboard);
-            // System.out.println();
         }
 
         System.out.println(change);
-        // PrintWriter output = new PrintWriter(new File("measurement.out"));
-        // output.println(change);
-        // output.close();
     }
 }
--- USACO/AlphaStar/Silver Advanced/Problem Set 5/tlines.java
+++ USACO/AlphaStar/Silver Advanced/Problem Set 5/tlines.java
@@ -1,3 +1,4 @@
+import java.sql.PreparedStatement;
 import java.util.*;
 
 public class tlines {
@@ -5,9 +6,91 @@ public class tlines {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         Coord[] coordinates = new Coord[N];
+
         for (int i = 0; i < N; i++) {
             coordinates[i] = new Coord(input.nextInt(), input.nextInt());
         }
+
+        input.close();
+
+        // for (Coord c: coordinates) System.out.println(c);
+        boolean shouldContinue = true;
+        HashSet<Integer> parallels = new HashSet<Integer>();
+        
+        // Case 1: All parallel (x = k)
+        if (shouldContinue) {
+            for (Coord c: coordinates) {
+                if (parallels.size() > 3) break;
+                parallels.add(c.x);
+            }
+            
+            if (parallels.size() <= 3) shouldContinue = false;
+        }
+        
+        parallels.clear();
+        
+        // Case 2: All parallel (y = k)
+        if (shouldContinue) {
+            for (Coord c: coordinates) {
+                if (parallels.size() > 3) break;
+                parallels.add(c.y);
+            }
+            
+            if (parallels.size() <= 3) shouldContinue = false;
+        }
+        
+        parallels.clear();
+        
+        // Case 3: Two parallel (x = k), one perpendicular (y = k)
+        HashMap<Integer, Integer> perpendicular = new HashMap<Integer, Integer>();
+
+        if (shouldContinue) {
+            for (Coord c: coordinates) {
+                if (!perpendicular.containsKey(c.y)) perpendicular.put(c.y, 0);
+                perpendicular.put(c.y, perpendicular.get(c.y) + 1);
+            }
+            int maxFrequency = 0, maxY = 0;
+            for (Map.Entry<Integer, Integer> entry: perpendicular.entrySet()) {
+                if (entry.getValue() > maxFrequency) {
+                    maxFrequency = entry.getValue();
+                    maxY = entry.getKey();
+                }
+            }
+            for (Coord c: coordinates) {
+                if (parallels.size() > 2) break;
+                if (c.y != maxY) parallels.add(c.x);
+            }
+
+            if (parallels.size() <= 2) shouldContinue = false;
+        }
+
+        perpendicular.clear();
+        parallels.clear();
+        // Case 4: Two parallel (y = k), one perpendicular (x = k)
+        if (shouldContinue) {
+            for (Coord c: coordinates) {
+                if (!perpendicular.containsKey(c.x)) perpendicular.put(c.x, 0);
+                perpendicular.put(c.x, perpendicular.get(c.x) + 1);
+            }
+            int maxFrequency = 0, maxX = 0;
+            for (Map.Entry<Integer, Integer> entry: perpendicular.entrySet()) {
+                if (entry.getValue() > maxFrequency) {
+                    maxFrequency = entry.getValue();
+                    maxX = entry.getKey();
+                }
+            }
+            for (Coord c: coordinates) {
+                if (parallels.size() > 2) break;
+                if (c.x != maxX) parallels.add(c.y);
+            }
+
+            if (parallels.size() <= 2) shouldContinue = false;
+        }
+
+        perpendicular.clear();
+        parallels.clear();
+
+        System.out.println(!shouldContinue ? 1 : 0);
     }
 
     public static class Coord {
