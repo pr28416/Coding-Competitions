dcd701c27ba4dd63bb724a03e333202a4692f215
Pranav Ramesh <pranav.ramesh1@gmail.com>
2020-12-02 15:42:40 -0500

    Finished flyingcow

--- USACO/AlphaStar/Silver Advanced/Problem Set 7/flyingcow.java
+++ USACO/AlphaStar/Silver Advanced/Problem Set 7/flyingcow.java
@@ -6,36 +6,38 @@ public class flyingcow {
         N = input.nextInt(); M = N-1;
         input.close();
 
-        visited = new boolean[N+1]; visited[0] = true;
+        visited = new boolean[N+4]; visited[0] = true;
 
-        goHome(N, 0);
+        Queue<Pair> queue = new LinkedList<Pair>();
+        queue.add(new Pair(N, 0));
+        Pair i;
+        
+        while (!queue.isEmpty()) {
+            i = queue.remove();
+
+            if (i.n < 1) continue;
+            else if (i.n == 1) M = Integer.min(i.m, M);
+            else if (i.m > M) continue;
+
+            else {
+                if (visited[i.n]) continue;
+                visited[i.n] = true;
+
+                if (i.n % 3 == 0) queue.add(new Pair(i.n/3, i.m+1));
+                if (i.n < N+3) queue.add(new Pair(i.n+1, i.m+1));
+                if (i.n > 0) queue.add(new Pair(i.n-1, i.m+1));
+            }
+        }
         System.out.println(M);
-        // 1 2 3 4 5
+        
+    }
+
+    public static class Pair {
+        int n, m;
+        public Pair(int a, int b) {n = a; m = b;}
     }
 
     static int N;
     static int M;
     static boolean[] visited;
-
-    public static void goHome(int pos, int m) {
-        for (int i = 0; i < m; i++) System.out.print("  ");
-        System.out.println(pos);
-        if (pos < 1) return;
-        else if (pos == 1) {
-            if (m < M) M = m;
-        }
-        else if (m > N) return;
-        else {
-            if (visited[pos]) return;
-            visited[pos] = true;
-            // Case 1: Use the Flying cow
-            if (pos % 3 == 0) goHome(pos/3, m+1);
-            // else {
-            // Case 2: Drag left
-            if (pos > 0) goHome(pos-1, m+1);
-            // Case 3: Drag right
-            if (pos < N) goHome(pos+1, m+1);
-            // }
-        }
-    }
 }
