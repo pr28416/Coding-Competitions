aea19943d7d199ecee0f89cc7a2759ce992c3d22
Pranav Ramesh <pranav.ramesh1@gmail.com>
2020-12-05 22:25:22 -0500

    Need to fix issue with input

--- USACO/AlphaStar/Silver Advanced/Problem Set 7/paintjob.java
+++ USACO/AlphaStar/Silver Advanced/Problem Set 7/paintjob.java
@@ -6,6 +6,7 @@ public class paintjob {
         
         int A = input.nextInt(), B = input.nextInt();
         int N = input.nextInt(), P = input.nextInt();
+
         int[] colors = new int[N];
         for (int i = 0; i < N; i++) {
             colors[i] = input.nextInt();
@@ -14,39 +15,32 @@ public class paintjob {
         input.close();
 
         Queue<tuple> queue = new LinkedList<tuple>();
-        Set<Integer> seen = new HashSet<Integer>();
+        int[] visited = new int[P];
+        visited[A % P] = 1;
+        int totalP = 1;
         queue.add(new tuple(A, 0));
 
-        int j = 0;
-
         while (queue.size() > 0) {
-            if (j > Integer.MAX_VALUE/2) {
-                System.out.println(-1);
-                break;
-            }
-            boolean notSeen = true;
-
             tuple item = queue.remove();
+
             if (item.a == B) {
                 System.out.println(item.b);
                 break;
             }
-
-            for (int i = 0; i < colors.length; i++) {
-                int n = item.a * colors[i] % P;
-                // if (!seen.contains(n)) {
-                    // seen.add(n);
-                    notSeen = false;
-                    queue.add(new tuple(n, item.b+1));
-                // }
-            }
-
-            if (notSeen) {
+            
+            if (totalP > P) {
                 System.out.println(-1);
                 break;
             }
-
-            j += 1;
+            
+            for (int i = 0; i < colors.length; i++) {
+                int n = item.a * colors[i] % P;
+                if (visited[n] == 0) {
+                    visited[n] = 1;
+                    totalP += 1;
+                    queue.add(new tuple(n, item.b+1));
+                }
+            }
         }
     }
 
